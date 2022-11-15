a8db01df36cc5520ea977120fb3df156a622fa94
Terry <terrybmochire@gmail.com>
2022-11-15 13:04:01 +0300

    Correct typo

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -126,7 +126,7 @@ class InventoryViewModel(private val itemDao: ItemDao): ViewModel() {
 class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(InventoryViewModel::class.java)){
-            @Suppress("UCHECKED_CAST")
+            @Suppress("UNCHECKED_CAST")
             return InventoryViewModel(itemDao) as T
         }
         throw IllegalArgumentException("Unknown ViewModel Class")
